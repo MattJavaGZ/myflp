ec91683468baff6df95ecb4d8e3e365b418761ea
MattJavaGZ <matek851@gmail.com>
2025-10-19 15:41:22 +0200

    liquibase update

---
+++ src/main/resources/db/testdata/0007_product2.sql
@@ -0,0 +1,13 @@
+INSERT INTO product (part_number, name, cc, retail_price_netto, retail_price_brutto)
+VALUES ('456', 'Vital5 | Forever Aloe Vera Gel', 1.035, 1302.4, 1406.6),
+       ('457', 'Vital5 | Forever Aloe Berry Nectar', 1.035, 1302.4, 1406.6),
+       ('458', 'Vital5 | Forever Aloe Peaches', 1.035, 1302.4, 1406.6),
+       ('459', 'Vital5 | Forever Freedom', 1.183, 1519.34, 1746.26),
+       ('659', 'DX4', 0.56, 734.45, 793.09),
+       ('475', 'C9 | Aloe Vera Gel & Lite Ultra Vanilla', 0.467, 601.9, 650.06),
+       ('476', 'C9 | Aloe Vera Gel & Lite Ultra Chocolate', 0.467, 601.9, 669.14),
+       ('625', 'C9 | Aloe Berry Nectar & Lite Ultra Vanilla', 0.467, 601.9, 650.06),
+       ('626', 'C9 | Aloe Berry Nectar & Lite Ultra Chocolate', 0.467, 601.9, 669.14),
+       ('528', 'F15 | Forever Lite Ultra Vanilla', 0.586, 755.37, 815.81),
+       ('529', 'F15 | Forever Lite Ultra Chocolate', 0.586, 755.37, 834.94),
+       ('9007', 'Zestaw Mini Touch of Forever', 1.0, 1200.86, 1477.06);

358c5459bb585910491151fb5a06df3e87096f45
MattJavaGZ <matek851@gmail.com>
2025-10-19 15:33:35 +0200

    word station update

--- src/main/resources/db/testdata/0001_work_station.sql
+++ src/main/resources/db/testdata/0001_work_station.sql
@@ -1,5 +1,6 @@
-insert into work_station (name, discount) values ('Preferred consumer', 5);
-insert into work_station (name, discount) values ('Asystent supervisor', 30);
-insert into work_station (name, discount) values ('Supervisor', 38);
-insert into work_station (name, discount) values ('Asystent menadżer', 43);
-insert into work_station (name, discount) values ('Menadżer', 48);
+INSERT INTO work_station (name, discount)
+VALUES ('Preferred consumer', 5),
+       ('Asystent supervisor', 30),
+       ('Supervisor', 38),
+       ('Asystent menadżer', 43),
+       ('Menadżer', 48);

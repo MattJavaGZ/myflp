e662869e8fc1bd9229cc88fe9e02ff35be03bb59
Matt <matek851@gmail.com>
2026-08-13 11:19:31 +0200

    add test users

--- src/main/resources/db/testdata/0005_test_users.sql
+++ src/main/resources/db/testdata/0005_test_users.sql
@@ -4,8 +4,3 @@ values
     ('Mateusz', 'Kowalski', 'admin@admin.pl', '{noop}admin', 'sdf435345jn', 3,true, '2025-07-11 14:30:00'),
     ('User','User', 'user@user.pl', '{noop}user', 'sdf4352323345jn', 2, true, '2025-08-11 14:30:00');
 
-insert into
-    user_roles (user_id, role_id)
-values
-    (1, 1),
-    (2,2);
